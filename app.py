33a89036e65b60128afbf5162ec576cb881ecaf5
ShireenKumar <shireen.kumar1304@gmail.com>
2026-01-19 13:15:36 -0500

    Coded home timeline

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import csv
 import time
+import random
 
 load_dotenv()
 
@@ -22,8 +23,8 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
-cursor.execute("TRUNCATE TABLE follows")
-conn.commit()
+# cursor.execute("TRUNCATE TABLE follows")
+# conn.commit()
 
 with open("follows.csv", "r") as f:
     reader = csv.DictReader(f)
@@ -52,5 +53,32 @@ tps = tweet_count / elapsed
 print(f"Inserted {tweet_count} tweets in {elapsed:.2f} seconds")
 print(f"Throughput: {tps:.2f} tweets/sec")
 
+# Grab the max and min
+cursor.execute("SELECT MIN(user_id), MAX(user_id) FROM users")
+min_user, max_user = cursor.fetchone()
+
+iterations = 1000 
+start_time = time.time()
+
+
+for _ in range(iterations):
+    user_id = random.randint(min_user, max_user)
+
+    # select top 10 posts from users followers 
+    # Join follows and tweets on followee_id and user_id
+    # Grab the followers with the user_id and order by timestamp
+    cursor.execute(
+        """
+        SELECT t.user_id, t.tweet_text, t.tweet_ts
+        FROM tweet t JOIN follow f ON t.user_id = f.followee_id
+        WHERE f.follower_id = %s
+        ORDER BY t.tweet_ts DESC
+        LIMIT 10
+    """,
+        (user_id,)
+    )
+
+    timeline = cursor.fetchall()
+end_time = time.time()
 cursor.close()
 conn.close()
